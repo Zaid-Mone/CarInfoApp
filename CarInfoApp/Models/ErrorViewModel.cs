a92c76aaf6498b5a00768609c4005ae25c0b9a42
zaidm <zaidm@SIMBA>
2025-07-13 23:39:00 +0300

    Add project files.

---
+++ CarInfoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarInfoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
